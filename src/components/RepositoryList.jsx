70b11936a03debfda2cac11eaedcac203dee607d
João Dias <joaovitorelf@gmail.com>
2023-08-14 17:56:57 -0300

    Criando o primeiro componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository = {
+    name: "Teste",
+    description: "Isso é um teste",
+    link: "github.com/Joo-Dias"
+}
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <RepositoryItem repository={repository} />
+            </ul>
+        </section>
+    )
+}
