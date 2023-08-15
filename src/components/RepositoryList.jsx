ba1c26497ca4a11bc5a61df4ee8a9dd9e837a109
João Dias <joaovitorelf@gmail.com>
2023-08-15 15:06:33 -0300

    Estilizando o repository-list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,20 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
 
 const repository = {
-    name: "Teste",
-    description: "Isso é um teste",
-    link: "github.com/Joo-Dias"
-}
+  name: "Teste",
+  description: "Isso é um teste",
+  link: "github.com/Joo-Dias",
+};
 
 export function RepositoryList() {
-    return (
-        <section className="repository-list">
-            <h1>Lista de repositórios</h1>
-            <ul>
-                <RepositoryItem repository={repository} />
-            </ul>
-        </section>
-    )
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
 }
