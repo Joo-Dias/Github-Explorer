fcae411a5963d7cfd2b52efcee904a2d32630596
João Dias <joaovitorelf@gmail.com>
2023-08-15 16:06:16 -0300

    Listando os repostórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,13 +3,6 @@ import { useEffect, useState } from "react";
 
 import "../styles/repositories.scss";
 
-
-const repository = {
-  name: "Teste",
-  description: "Isso é um teste",
-  link: "github.com/Joo-Dias",
-};
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
@@ -24,7 +17,9 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+            return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
